8f7e4be7bb1bf4f10352563a4958ad05cd382f8d
Nicholas Alexander Limit <nicholas.alex.limit@gmail.com>
2022-04-08 23:38:31 +0700

    Server: updated comments, rearranged some code

--- Server.cpp
+++ Server.cpp
@@ -9,15 +9,15 @@
 
 #pragma comment (lib, "Ws2_32.lib")
 
-#define DEFAULT_PORT "8686"
+#define DEFAULT_PORT "8686" // TODO change default port
 #define DEFAULT_BUFLEN 1024
 
-Server::Server(void) {
+Server::Server(void)
+{
 	// initialize Winsock 2.2
 	WSADATA wsaData;
 	int startupStatus = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (startupStatus != 0)
-	{
+	if (startupStatus != 0) {
 		printf("WSAStartup failed: %d\n", startupStatus);
 		exit(1);
 	}
@@ -27,12 +27,12 @@ Server::Server(void) {
 	ZeroMemory(&hints, sizeof(hints));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP; /* Establish TCP connection */
+	hints.ai_protocol = IPPROTO_TCP;
 	hints.ai_flags = AI_PASSIVE;
 
-	// Resolve the server address and port
+	// resolve the local address and port for the server
 	struct addrinfo* result = NULL;
-	int getaddrStatus = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result); // TODO change default port
+	int getaddrStatus = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
 	if (getaddrStatus != 0) {
 		printf("getaddrinfo failed with error: %d\n", getaddrStatus);
 		WSACleanup();
@@ -52,7 +52,6 @@ Server::Server(void) {
 	bool isNonblocking = false; // TODO revisit later, depending on game architecture
 	if (isNonblocking) {
 		// set the mode of the socket to be nonblocking
-
 		u_long iMode = 1;
 		int ioctlStatus = ioctlsocket(ListenSocket, FIONBIO, &iMode);
 		if (ioctlStatus == SOCKET_ERROR) {
@@ -63,7 +62,7 @@ Server::Server(void) {
 		}
 	}
 
-	// Setup the TCP listening socket
+	// bind passive socket to the local address
 	int bindStatus = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
 	if (bindStatus == SOCKET_ERROR) {
 		printf("bind failed with error: %d\n", WSAGetLastError());
@@ -86,9 +85,10 @@ Server::Server(void) {
 	}
 }
 
-Server::~Server(void) {
+Server::~Server(void)
+{
 	// shutdown the send half of the connection since no more data will be sent
-		int shutdownCode = shutdown(ClientSocket, SD_SEND);
+	int shutdownCode = shutdown(ClientSocket, SD_SEND);
 	if (shutdownCode == SOCKET_ERROR) {
 		printf("shutdown failed: %d\n", WSAGetLastError());
 		closesocket(ClientSocket);
@@ -104,44 +104,44 @@ Server::~Server(void) {
 void Server::mainLoop(void)
 {
 	ClientSocket = INVALID_SOCKET;
-	ClientSocket = accept(ListenSocket, NULL, NULL);
-	if (ClientSocket != INVALID_SOCKET)
-	{
-		// disable Nagle on the client's socket
-		char value = 1;
-		setsockopt(ClientSocket, IPPROTO_TCP, TCP_NODELAY, &value, sizeof(value));
-
-		// LATER: insert new client into session id table
-		// sessions.insert( pair<unsigned int, SOCKET>(id, ClientSocket) );
+	ClientSocket = accept(ListenSocket, NULL, NULL); // TODO ClientSocket handling can be multithreaded
+	if (ClientSocket == INVALID_SOCKET) {
+		printf("accept failed: %d\n", WSAGetLastError());
+		return; // TODO error condition for this
 	}
 
+	// disable Nagle on the client socket
+	char value = 1;
+	setsockopt(ClientSocket, IPPROTO_TCP, TCP_NODELAY, &value, sizeof(value));
+
+	// TODO insert new client into session id table
+	// sessions.insert(pair<unsigned int, SOCKET>(id, ClientSocket));
+
 	char buffer[DEFAULT_BUFLEN];
-	while (true) {
-		/* Receive data */
-		int data_length;
-		data_length = recv(ClientSocket, buffer, DEFAULT_BUFLEN, 0);
-		if (data_length == 0) {
+	while (true) { // TODO 
+		// receive data from client
+		int recvStatus = recv(ClientSocket, buffer, DEFAULT_BUFLEN, 0);
+		if (recvStatus == SOCKET_ERROR) {
+			printf("recv failed: %d\n", WSAGetLastError());
+			continue;
+		}
+		else if (recvStatus == 0) {
 			printf("Connection closed\n");
 			closesocket(ClientSocket);
+			return; // TODO only terminate for this client, not others
 		}
-		else if (data_length < 0) {
-			// no data received
-			continue;
-			// TODO nonblocking stuff, deal with later
+		else {
+			printf("Server bytes received: %ld\n", recvStatus);
 		}
 
-		if (data_length > 0) { // TODO remove clause when using non-blocking
-			//echo back the data recieve to the client 
-			int sendStatus = send(ClientSocket, buffer, DEFAULT_BUFLEN, 0);
-			if (sendStatus == SOCKET_ERROR) {
-				printf("send failed: %d\n", WSAGetLastError());
-				closesocket(ClientSocket);
-				WSACleanup();
-				exit(1);
-			}
-			else {
-				printf("Bytes Sent: %ld\n", sendStatus);
-			}
+		// echo back the data received to the client 
+		int sendStatus = send(ClientSocket, buffer, recvStatus, 0);
+		if (sendStatus == SOCKET_ERROR) {
+			printf("send failed: %d\n", WSAGetLastError());
+			return; // TODO ideally retry transmission
+		}
+		else {
+			printf("Server bytes sent: %ld\n", sendStatus);
 		}
 	}
 }
