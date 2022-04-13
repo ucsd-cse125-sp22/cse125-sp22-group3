c6d4b55560be173a1666fe4d96bc639cc6bd56cc
jakob <jgetzel@ucsd.edu>
2022-04-13 00:04:53 -0700

    Started Basic PhysicsEngine Implementation

---
+++ CollidableAABB.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <glm/vec2.hpp>
+#include "Collidable.h"
+
+class CollidableAABB final : public Collidable
+{
+public:
+    CollidableAABB();
+    CollidableAABB(const glm::vec2& min, const glm::vec2& max, bool is_static = true);
+
+    glm::vec2 minimum{};
+    glm::vec2 maximum{};
+    bool collider_is_static{};
+     
+    bool CollidesWith(Collidable* other_collidable) const override;
+    CollidableAABB* BoundingBox() override;
+
+    //TODO Seperate "Collider Shapes" from "Collidable Game Entities"
+    //This object is a Collider, so we don't need this function
+    void OnCollide() override {} 
+
+    Collidable::Shape GetColliderShape() override;
+    bool GetColliderIsStatic() override;
+    
+    static CollidableAABB* SurroundingBox(const CollidableAABB& first, const CollidableAABB& second);
+};
