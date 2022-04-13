afbef954717e376e413d536173ab9d67ba0ae089
jakob <jgetzel@ucsd.edu>
2022-04-13 00:04:53 -0700

    Started Basic PhysicsEngine Implementation

--- Collidable.h
+++ Collidable.h
@@ -1,4 +1,16 @@
-class Collidable {
-	public:
-		virtual void onCollide() {};
+#pragma once
+
+class CollidableAABB;
+
+class Collidable
+{
+public:
+    virtual ~Collidable() = default;
+    virtual void OnCollide() = 0;
+    virtual bool CollidesWith(Collidable* other_collidable) const = 0;
+    virtual CollidableAABB* BoundingBox() = 0;
+
+    virtual enum Shape{CIRCLE, AABB_SHAPE} GetColliderShape() = 0;
+    virtual bool GetColliderIsStatic() = 0; // Is Object Static? (Stationary)
 };
+
