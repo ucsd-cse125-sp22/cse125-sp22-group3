e64b13cca15d6b36fdb4634c64a4b5a360d02dfd
jakob <jgetzel@ucsd.edu>
2022-04-13 00:04:53 -0700

    Started Basic PhysicsEngine Implementation

---
+++ PhysicsEngine.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "Collidable.h"
+#include <vector>
+
+class BVHNode
+{
+    
+};
+
+class PhysicsEngine
+{
+public:
+    explicit PhysicsEngine(std::vector<Collidable*> collidables);
+
+    void AddCollidable(Collidable* collidable);
+    void Compute();
+
+private:
+    std::vector<Collidable*> static_collidables_;
+    std::vector<Collidable*> moving_collidables_;
+
+    BVHNode root_node;
+};
