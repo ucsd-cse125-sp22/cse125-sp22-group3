d5fc13bb42638926127dd199eef667a389bf93fd
jakob <jgetzel@ucsd.edu>
2022-04-13 17:22:12 -0700

    Added Collider class (different from Collidable)

---
+++ Collider.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "Collidable.h"
+
+class Collider
+{
+public:
+    virtual bool CollidesWith(Collidable* other_collidable) const = 0;
+    virtual ColliderAABB* BoundingBox() = 0;
+
+    virtual enum Shape{CIRCLE, AABB_SHAPE} GetColliderShape() = 0;
+    virtual bool GetColliderIsStatic() = 0; // Is Object Static? (Stationary)
+};
