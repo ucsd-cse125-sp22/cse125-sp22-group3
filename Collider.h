8de83859784247de75d2eb0bba97bd7f1e4a1aa9
jakob <jgetzel@ucsd.edu>
2022-04-13 17:22:12 -0700

    Added Collider class (different from Collidable)

--- Collider.h
+++ Collider.h
@@ -9,5 +9,4 @@ public:
 
     virtual enum Shape{CIRCLE, AABB_SHAPE} GetColliderShape() = 0;
     virtual bool GetColliderIsStatic() = 0; // Is Object Static? (Stationary)
-    
 };
--- ColliderAABB.cpp
+++ ColliderAABB.cpp
@@ -14,7 +14,6 @@ bool ColliderAABB::CollidesWith(Collider* other_collider) const
     //TODO Perform a sub-case for AABB - AABB collision
 
     //TODO Perform a sub-case for AABB - Circle collision
-    
     return false;
 }
 ColliderAABB* ColliderAABB::BoundingBox()
