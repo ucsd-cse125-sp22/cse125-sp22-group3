7216e0117bc9797dc12d3ae535da7f2e703b19f9
jakob <jgetzel@ucsd.edu>
2022-04-13 18:30:59 -0700

    Added ColliderCircle

---
+++ ColliderCircle.cpp
@@ -0,0 +1,34 @@
+﻿#include "ColliderCircle.h"
+#include "ColliderAABB.h"
+
+ColliderCircle::ColliderCircle(const glm::vec2 center, const float radius, const bool is_static)
+{
+    this->center = center;
+    this->radius = radius;
+    this->collider_is_static = is_static;
+}
+
+bool ColliderCircle::CollidesWith(Collider* other_collider) const
+{
+    //TODO Perform a sub-case for Circle - Circle collision
+
+    
+    //TODO Perform a sub-case for Circle - AABB collision
+
+    
+}
+
+ColliderAABB* ColliderCircle::BoundingBox()
+{
+    return new ColliderAABB(center - radius, center + radius, collider_is_static);
+}
+
+Collider::Shape ColliderCircle::GetColliderShape()
+{
+    
+}
+
+bool ColliderCircle::GetColliderIsStatic()
+{
+    return collider_is_static;
+}
--- GameManager.h
+++ GameManager.h
@@ -3,7 +3,6 @@
 
 #include "Player.h"
 #include "GameEntity.h"
-#include "../../../../../Program Files (x86)/Microsoft Visual Studio/2019/BuildTools/VC/Tools/MSVC/14.29.30133/include/chrono"
 
 class GameManager : GameEntity, Drawable
 {
