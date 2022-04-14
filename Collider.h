5df6e6b77fdd15408ae2ad66b6f4de777f955d56
jakob <jgetzel@ucsd.edu>
2022-04-13 18:30:59 -0700

    Added ColliderCircle

--- Collider.h
+++ Collider.h
@@ -4,9 +4,10 @@
 class Collider
 {
 public:
-    virtual bool CollidesWith(Collidable* other_collidable) const = 0;
+    virtual bool CollidesWith(Collider* other_collidable) const = 0;
     virtual ColliderAABB* BoundingBox() = 0;
 
     virtual enum Shape{CIRCLE, AABB_SHAPE} GetColliderShape() = 0;
     virtual bool GetColliderIsStatic() = 0; // Is Object Static? (Stationary)
+    
 };
---
+++ ColliderCircle.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <glm/vec2.hpp>
+
+#include "Collider.h"
+
+class ColliderCircle : public Collider
+{
+public:
+    ColliderCircle();
+    ColliderCircle(glm::vec2 center, float radius, bool is_static = true);
+
+    glm::vec2 center{};
+    float radius{};
+    bool collider_is_static{};
+     
+    bool CollidesWith(Collider* other_collider) const override;
+    ColliderAABB* BoundingBox() override;
+    
+    Collider::Shape GetColliderShape() override;
+    bool GetColliderIsStatic() override;
+};
