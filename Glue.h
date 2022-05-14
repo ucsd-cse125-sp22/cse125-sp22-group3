e6a7bb6fc7a2aa6912b8d7b2fdb070128a51f1a8
Nicholas Alexander Limit <nicholas.alex.limit@gmail.com>
2022-05-14 12:32:24 -0700

    Glue on ground now works

--- Glue.h
+++ Glue.h
@@ -7,7 +7,6 @@ private:
 	float pickupHeight = -2; // TODO hello
 	float dropHeight = -4; // TODO danica
 	glm::mat4 hold_transformation_{}; // TODO ici
-	Player* playerBeingTrapped;
 
 public:
 	 Glue();
@@ -20,10 +19,6 @@ public:
 	void  Glue::OnDrop() override;
 	glm::mat4  Glue::GetHoldTransform() override;
 
-	// TODO SOMETHING IM DRUNK
-	//void OnCollide(PhysicsObject* object) override;
-	
-	void FixedUpdate();
 	bool isOnGround= false;
 };
 
